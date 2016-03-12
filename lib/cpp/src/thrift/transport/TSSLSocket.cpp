bc5c4ab145d40554a7b6e501c86e1d30bfeb8886
Nobuaki Sukegawa <nsuke@apache.org>
2016-03-13 08:55:38 +0900

    THRIFT-3736 C++ library build fails if OpenSSL does not surrpot SSLv3
    This closes #944
    
    Reviewed-by: @rcc

--- lib/cpp/src/thrift/transport/TSSLSocket.cpp
+++ lib/cpp/src/thrift/transport/TSSLSocket.cpp
@@ -141,10 +141,12 @@ SSLContext::SSLContext(const SSLProtocol& protocol) {
   {
     ctx_ = SSL_CTX_new(SSLv23_method());
   }
+#ifndef OPENSSL_NO_SSL3
   else if(protocol == SSLv3)
   {
     ctx_ = SSL_CTX_new(SSLv3_method());
   }
+#endif
   else if(protocol == TLSv1_0)
   {
     ctx_ = SSL_CTX_new(TLSv1_method());
